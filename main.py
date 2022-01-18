3f5fee5303e8e40657bf36c6af4ffceb4605149d
KESHA <trxllhost@gmail.com>
2022-01-18 02:24:31 +0200

    test

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-print("Hello world")
+action = input("Что делаем? (+, -): ")
+a = float(input("Введи первое число: "))
+b = float(input("Введи второе число: "))
+
+if action == "+":
+    c = a + b
+    print("Результат: " + str(c))
+elif action == "-":
+    c = a - b
+    print("Результат: " + str(c))
+else:
+    print("Неверная операция")
+
